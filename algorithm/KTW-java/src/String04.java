b4b2c1c01a8adfa8cd9e5b784f2acd55995d9953
heejun <heejjuunn@gmail.com>
2021-12-08 02:03:45 +0900

    String04(단어 뒤집기) solved

---
+++ algorithm/KTW-java/src/String04.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+//문장 속 가장 긴 단어 찾기
+public class String04 {
+    public ArrayList<String> solution(int n, String[] str){
+        ArrayList<String> answer = new ArrayList<>();
+        for(String s : str){
+            StringBuffer sb = new StringBuffer(s);
+            String reversedStr = sb.reverse().toString();
+            answer.add(reversedStr);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        String04 T = new String04();
+        String[] strings = {"good","Time","Big"};
+        System.out.println(T.solution(3,strings));
+    }
+}
---
+++ algorithm/KTW-java/src/String04_1.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+//문장 속 가장 긴 단어 찾기
+//String.valueOf()
+public class String04_1 {
+    public ArrayList<String> solution(int n, String[] str){
+        ArrayList<String> answer = new ArrayList<>();
+        for(String x :str){
+            char[] s = x.toCharArray();
+            int left = 0;
+            int right = x.length() -1;
+            while(left<right){
+                char tmp = s[left];
+                s[left] = s[right];
+                s[right] = tmp;
+                left++;
+                right--;
+            }
+            String tmp = String.valueOf(s);
+            answer.add(tmp);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        String04_1 T = new String04_1();
+        String[] strings = {"good","Time","Big"};
+        System.out.println(T.solution(3,strings));
+    }
+}
