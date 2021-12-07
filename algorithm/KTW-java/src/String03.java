bb7298fc962e466255c000ba0b4fdb2aeb7eb6d1
heejun <heejjuunn@gmail.com>
2021-12-08 01:49:08 +0900

    String03(가장 긴단어 찾기) solved

---
+++ algorithm/KTW-java/src/String03.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+//문장 속 가장 긴 단어 찾기
+public class String03 {
+    public String solution(String str){
+        String[] strings = str.split(" ");
+        int max = 0;
+        String answer = "";
+        for(String s : strings){
+            int length = s.length();
+            if(length > max)  {
+                max = Math.max(length,max);
+                answer = s;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        String03 T = new String03();
+        System.out.println(T.solution("it is time to study"));
+    }
+}
